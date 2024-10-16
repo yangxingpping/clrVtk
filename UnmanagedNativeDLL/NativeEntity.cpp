7ab0a68a3710fa8c701c34ee0f437433f7def399
youngxp <alqaz@outlook.com>
2024-10-16 21:07:36 +0800

    添加部分vtk代码

--- UnmanagedNativeDLL/NativeEntity.cpp
+++ UnmanagedNativeDLL/NativeEntity.cpp
@@ -1,6 +1,10 @@
 #include "NativeEntity.h"
 #include <string>
 #include <iostream>
+#include <array>
+
+#include "vtkPolyData.h"
+#include "vtkFloatArray.h"
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #include <windows.h>
@@ -8,10 +12,7 @@
 #pragma managed(push, off)
 #endif
 
-BOOL APIENTRY DllMain(
-	HMODULE		/*hModule*/,
-	DWORD		ul_reason_for_call,
-	LPVOID		/*lpReserved*/)
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD	ul_reason_for_call, LPVOID /*lpReserved*/)
 {
 	switch (ul_reason_for_call)
 	{
@@ -28,10 +29,48 @@ BOOL APIENTRY DllMain(
 #pragma managed(pop)
 #endif
 
-
 NativeEntity::NativeEntity()
 {
+	mPolyData = vtkPolyData::New();
+}
 
+NativeEntity::~NativeEntity()
+{
+	if (mPolyData != nullptr)
+	{
+		mPolyData->Delete();
+		mPolyData = nullptr;
+	}
+}
+
+bool NativeEntity::Init()
+{
+	std::array<std::array<double, 3>, 8> pts = { { { { 0, 0, 0 } }, { { 1, 0, 0 } }, { { 1, 1, 0 } },
+	{ { 0, 1, 0 } }, { { 0, 0, 1 } }, { { 1, 0, 1 } }, { { 1, 1, 1 } }, { { 0, 1, 1 } } } };
+	// The ordering of the corner points on each face.
+	std::array<std::array<vtkIdType, 4>, 6> ordering = { { { { 0, 1, 2, 3 } }, { { 4, 5, 6, 7 } },
+	  { { 0, 1, 5, 4 } }, { { 1, 2, 6, 5 } }, { { 2, 3, 7, 6 } }, { { 3, 0, 4, 7 } } } };
+
+	vtkNew<vtkPoints> points;
+	vtkNew<vtkCellArray> polys;
+	vtkNew<vtkFloatArray> scalars;
+
+	// Load the point, cell, and data attributes.
+	for (auto i = 0ul; i < pts.size(); ++i)
+	{
+		points->InsertPoint(i, pts[i].data());
+		scalars->InsertTuple1(i, i);
+	}
+	for (auto&& i : ordering)
+	{
+		polys->InsertNextCell(vtkIdType(i.size()), i.data());
+	}
+
+	// We now assign the pieces to the vtkPolyData.
+	mPolyData->SetPoints(points);
+	mPolyData->SetPolys(polys);
+	//mPolyData->GetPointData()->SetScalars(scalars);
+	return true;
 }
 
 int NativeEntity::GetInt()
@@ -39,9 +78,13 @@ int NativeEntity::GetInt()
 	return mInt;
 }
 
+vtkPolyData* NativeEntity::GetPolyData()
+{
+	return mPolyData;
+}
+
 
 void NativeEntity::SetInt(int nInt)
 {
 	mInt = nInt;
 }
-
--- UnmanagedNativeDLL/NativeEntity.h
+++ UnmanagedNativeDLL/NativeEntity.h
@@ -8,17 +8,25 @@
 #define EXAMPLEUNMANAGEDDLL_API __declspec(dllimport)
 #endif
 
+class vtkPolyData;
+
 class EXAMPLEUNMANAGEDDLL_API NativeEntity
 {
 public:
 	NativeEntity();
+	NativeEntity(const NativeEntity& ref) = delete;
+	NativeEntity(const NativeEntity&& ref) = delete;
+	~NativeEntity();
+	bool Init();
 
 	//gets
 	int GetInt();
+	vtkPolyData* GetPolyData();
 
 	//sets
 	void SetInt(int nInt);
 
 private:
 	int mInt{ 0 };
+	vtkPolyData* mPolyData{ nullptr };
 };
